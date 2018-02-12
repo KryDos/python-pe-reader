9345b9c3e33d4d09dace4197fc5506d3cbec070c
Ruslan Bekenev <furyinbox@gmail.com>
2018-02-12 08:23:07 +0300

    show number of sections

--- pereader.py
+++ pereader.py
@@ -1,5 +1,6 @@
 import sys
 from pe_machine import PeMachine
+from sections_count import SectionsCount
 
 class PeReader():
     def __init__(self, file_path):
@@ -9,8 +10,16 @@ class PeReader():
         machine = PeMachine(self.file)
         return machine.getValue()
 
+    def getNumberOfSections(self):
+        sections_counter = SectionsCount(self.file)
+        return sections_counter.getValue()
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.file.close()
+
+    def printInfo(self):
+        print("Machine Type:\t" + self.getMachine())
+        print("Number of sections:\t" + self.getNumberOfSections())
 
 if len(sys.argv) == 1:
     print("File path is required")
@@ -18,4 +27,4 @@ if len(sys.argv) == 1:
 
 pe_reader = PeReader(sys.argv[1]);
 print("\n")
-print("Machine Type:\t" + pe_reader.getMachine())
+pe_reader.printInfo()
